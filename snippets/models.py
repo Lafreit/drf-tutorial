01084b1af1c09eaa1ccb864da85d8e48a2545790
Luiz Antonio de Freitas <lafreitas2@yahoo.com.br>
2026-01-10 23:21:27 -0300

    requisições e respostas

--- snippets/models.py
+++ snippets/models.py
@@ -1,5 +1,8 @@
+# snippets/models.py
 from django.db import models
-from pygments.lexers import get_all_lexers
+from pygments import highlight # For code highlighting
+from pygments.formatters.html import HtmlFormatter # For code highlighting
+from pygments.lexers import get_all_lexers, get_lexer_by_name # For code highlighting
 from pygments.styles import get_all_styles
 
 # Create your models here.
@@ -18,9 +21,27 @@ class Snippet(models.Model):
     style = models.CharField(
         choices=STYLE_CHOICES, default='friendly', max_length=100
     )
+    owner = models.ForeignKey(
+        'auth.User', related_name='snippets', on_delete=models.CASCADE
+    ) # Owner of the snippet
+    highlighted = models.TextField() # For storing highlighted code
 
     class Meta:
-        ordering = ["created"]
+        ordering = ("created",)
+
+    def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(
+            style=self.style, linenos=linenos, full=True, **options
+        )
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title

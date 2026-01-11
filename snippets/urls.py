b4571fd2422c36cc0e751ec2b5b88eaceb076911
Luiz Antonio de Freitas <lafreitas2@yahoo.com.br>
2026-01-11 13:13:08 -0300

    Adicionando endpoints para nosso User Models

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,8 +4,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('snippets/', views.SnippetList.as_view()), # List and create view for snippets
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()), # Detail view for snippets
+    path('users/', views.UserList.as_view()), # List and create view for users
+    path('users/<int:pk>/', views.UserDetail.as_view()), # Detail view for users
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -1,13 +1,23 @@
 # snippets/views.py
+from django.contrib.auth.models import User # For user model
 from rest_framework import generics
+
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer # Import UserSerializer
 
 # Create your views here.
-class SnippetList(generics.ListCreateAPIView):
+class SnippetList(generics.ListCreateAPIView): # View for listing and creating snippets
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView): # View for snippet detail
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView): # View for listing users
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView): # View for user detail
+    queryset = User.objects.all()
+    serializer_class = UserSerializer

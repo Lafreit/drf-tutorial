510386f19635796f188e1bfa6a87c7231e66d4c9
Luiz Antonio de Freitas <lafreitas2@yahoo.com.br>
2026-01-12 19:39:05 -0300

    Endpoint raiz da API

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,10 +4,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()), # List and create view for snippets
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()), # Detail view for snippets
-    path('users/', views.UserList.as_view()), # List and create view for users
-    path('users/<int:pk>/', views.UserDetail.as_view()), # Detail view for users
+    path('snippets/', views.SnippetList.as_view(), name='snippet-list'), # List and create view for snippets
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'), # Detail view for snippets
+    path('users/', views.UserList.as_view(), name='user-list'), # List and create view for users
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'), # Detail view for users
+    path('', views.api_root, name='api-root'), # Root API endpoint
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -1,12 +1,21 @@
 # snippets/views.py
 from django.contrib.auth.models import User # For user model
 from rest_framework import generics, permissions # For generic views and permissions
+from rest_framework.decorators import api_view # For API view decorator
+from rest_framework.response import Response # For API responses
+from rest_framework.reverse import reverse # For reversing URLs
 
 from .models import Snippet # Import Snippet model
 from .permissions import IsOwnerOrReadOnly # Import custom permission
 from .serializers import SnippetSerializer, UserSerializer # Import UserSerializer
 
 # Create your views here.
+@api_view(['GET']) # API view for root endpoint
+def api_root(request, format=None): # Function for API root
+    return Response({ # Return a response with links to users and snippets
+        'users': reverse('user-list', request=request, format=format), # Link to user list
+        'snippets': reverse('snippet-list', request=request, format=format) # Link to snippet list
+    })
 class SnippetList(generics.ListCreateAPIView): # View for listing and creating snippets
     queryset = Snippet.objects.all() # Queryset for all snippets
     serializer_class = SnippetSerializer # Serializer for Snippet model
